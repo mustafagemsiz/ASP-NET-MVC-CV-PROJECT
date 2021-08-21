8991c716b4b164745b207c7b8662e228d27dfc85
Mustafa Gemsiz <birhatgemsiz@hotmail.com>
2021-08-21 21:44:58 +0300

    update

--- CV_PROJECT/CV_PROJECT/Controllers/IndexController.cs
+++ CV_PROJECT/CV_PROJECT/Controllers/IndexController.cs
@@ -13,8 +13,49 @@ namespace CV_PROJECT.Controllers
         public ActionResult Index()
         {
             var deger = db.TBL_HAKKIMDA.ToList();
-
             return View(deger);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deger = db.TBL_DENEYIM.ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Egitim()
+        {
+            var deger = db.TBL_EGITIM.ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Yetenek()
+        {
+            var deger = db.TBL_YETENEK.ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Hobi()
+        {
+            var deger = db.TBL_HOBI.ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Sertifika()
+        {
+            var deger = db.TBL_SERTIFIKA.ToList();
+            return PartialView(deger);
+        }
+        [HttpGet]
+        public PartialViewResult Iletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Iletisim(TBL_ILETISIM t)
+        {
+            t.TARIH = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TBL_ILETISIM.Add(t);
+            db.SaveChanges();
+            return PartialView();
         }
     }
 }
