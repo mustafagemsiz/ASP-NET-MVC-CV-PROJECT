488169cae667d81d51d93196acf1b3a2b470b474
Mustafa Gemsiz <birhatgemsiz@hotmail.com>
2021-08-22 21:27:46 +0300

    update

---
+++ CV_PROJECT/CV_PROJECT/Repositories/DeneyimRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CV_PROJECT.Models.Entity;
+using CV_PROJECT.Repostories;
+
+namespace CV_PROJECT.Repositories
+{
+    public class DeneyimRepository:GenericRepository<TBL_DENEYIM>
+    {
+
+    }
+}
---
+++ CV_PROJECT/CV_PROJECT/Repositories/IletisimRepository.cs
@@ -0,0 +1,14 @@
+﻿using CV_PROJECT.Models.Entity;
+using CV_PROJECT.Repostories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CV_PROJECT.Repositories
+{
+    public class IletisimRepository : GenericRepository<TBL_ILETISIM>
+    {
+
+    }
+}
